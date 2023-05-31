36ae5d62ec012ae879aa1b3f6db8f966e86d5f2b
dominika232323 <dominikaboguszewska62@gmail.com>
2023-06-01 01:35:19 +0200

    Test push

--- StackMachine/StackMachineUnitTest/StackUnitTest.cpp
+++ StackMachine/StackMachineUnitTest/StackUnitTest.cpp
@@ -25,5 +25,26 @@ namespace StackUnitTest
 			Assert::AreEqual(size_t(1), s.size());
 			Assert::IsFalse(s.isEmpty());
 		}
+		TEST_METHOD(push_toEmpty)
+		{
+			Stack<int> s;
+			s.push(6);
+			std::vector<int> expected = { 6 };
+			Assert::IsTrue(expected == s.getStack());
+			Assert::AreEqual(size_t(1), s.size());
+			Assert::IsFalse(s.isEmpty());
+		}
+		TEST_METHOD(push)
+		{
+			Stack<int> s(6);
+			s.push(12);
+			s.push(16);
+			s.push(14);
+			s.push(25);
+			std::vector<int> expected = { 6, 12, 16, 14, 25 };
+			Assert::IsTrue(expected == s.getStack());
+			Assert::AreEqual(size_t(5), s.size());
+			Assert::IsFalse(s.isEmpty());
+		}
 	};
 }
