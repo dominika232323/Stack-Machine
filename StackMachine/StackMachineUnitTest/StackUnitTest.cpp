86a01b03a1644d239dd20eaeaf066157604e59d9
dominika232323 <dominikaboguszewska62@gmail.com>
2023-06-01 01:32:43 +0200

    Test constructor

--- StackMachine/StackMachineUnitTest/StackUnitTest.cpp
+++ StackMachine/StackMachineUnitTest/StackUnitTest.cpp
@@ -17,5 +17,13 @@ namespace StackUnitTest
 			Assert::AreEqual(size_t(0), s.size());
 			Assert::IsTrue(s.isEmpty());
 		}
+		TEST_METHOD(constructorValue)
+		{
+			Stack<int> s(6);
+			std::vector<int> expected = { 6 };
+			Assert::IsTrue(expected == s.getStack());
+			Assert::AreEqual(size_t(1), s.size());
+			Assert::IsFalse(s.isEmpty());
+		}
 	};
 }
