ef7df1490226f2c8e1514867a0438e65c09c1923
dominika232323 <dominikaboguszewska62@gmail.com>
2023-06-01 01:21:21 +0200

    Define stack methods

--- StackMachine/StackMachineLib/Stack.cpp
+++ StackMachine/StackMachineLib/Stack.cpp
@@ -24,3 +24,54 @@ public:
 	T top();
 };
 
+template<typename T>
+Stack<T>::Stack()
+{
+}
+
+template<typename T>
+Stack<T>::Stack(T value)
+{
+	push(value);
+}
+
+template<typename T>
+Stack<T>::~Stack()
+{
+}
+
+template<typename T>
+std::vector<T> Stack<T>::getStack()
+{
+	return stack;
+}
+
+template<typename T>
+size_t Stack<T>::size()
+{
+	return stack.size();
+}
+
+template<typename T>
+bool Stack<T>::isEmpty()
+{
+	return size() == 0;
+}
+
+template<typename T>
+void Stack<T>::push(T value)
+{
+	stack.push_back(value);
+}
+
+template<typename T>
+T Stack<T>::pop()
+{
+	return stack.pop_back();
+}
+
+template<typename T>
+T Stack<T>::top()
+{
+	return stack.end();
+}
