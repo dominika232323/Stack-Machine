2cab632a40a7fa6a6b0ba9031774baafef0c42d0
dominika232323 <dominikaboguszewska62@gmail.com>
2023-06-03 11:18:50 +0200

    Define execute in OperationSubtraction

--- StackMachine/StackMachineLib/OperationSubtraction.cpp
+++ StackMachine/StackMachineLib/OperationSubtraction.cpp
@@ -8,3 +8,11 @@ class OperationSubtraction : public Operation<T>
 public:
 	void execute(Stack<T>& s);
 };
+
+template<typename T>
+void OperationSubtraction<T>::execute(Stack<T>& s)
+{
+	T a = topAndPopOnStack(s);
+	T b = topAndPopOnStack(s);
+	s.push(a - b);
+}
