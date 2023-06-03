fd2a80405fa7f61adff0204ffd273fe2db2e2265
dominika232323 <dominikaboguszewska62@gmail.com>
2023-06-04 00:05:14 +0200

    Start writing app

--- StackMachine/StackMachineApp/StackMachineApp.cpp
+++ StackMachine/StackMachineApp/StackMachineApp.cpp
@@ -4,5 +4,46 @@
 
 int main()
 {
+	std::cout << "Welcome to stack machine!" << std::endl;
+	std::cout << "Choose 1 to read stack machine from file." << std::endl;
+	std::cout << "Choose 2 to start a new stack machine." << std::endl;
+	std::cout << "Choose 3 to exit." << std::endl;
+	std::cout << "I choose: ";
+
+	int choice;
+	std::cin >> choice;
+
+	switch (choice)
+	{
+	case 1:
+		std::cout << "Give path to file with your stack: ";
+
+		std::string filename;
+		std::cin >> filename;
+
+		StackMachine<int> stackMachine(filename);
+		break;
+
+	case 2:
+		std::cout << "Choose 1 to operate on ints." << std::endl;
+		std::cout << "Choose 2 to operate on doubles." << std::endl;
+
+		int choice2;
+		std::cin >> choice2;
+
+		switch (choice2)
+		{
+		case 1:
+			StackMachine<int> stackMachine;
+			break;
+
+		case 2:
+			StackMachine<double> stackMachine;
+			break;
+
+		default:
+
+		}
+	}
 
 }
