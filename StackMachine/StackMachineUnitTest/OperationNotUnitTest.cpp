a28d529e64dd6a7a2a514d46279754f7ba5d7e95
dominika232323 <dominikaboguszewska62@gmail.com>
2023-06-03 23:35:29 +0200

    Add operation not

---
+++ StackMachine/StackMachineUnitTest/OperationNotUnitTest.cpp
@@ -0,0 +1,58 @@
+#include "CppUnitTest.h"
+#include "../StackMachineLib/OperationNot.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace OperationNotUnitTest
+{
+	TEST_CLASS(OperationNotUnitTest)
+	{
+	public:
+
+		TEST_METHOD(execute_ZeroElementsOnStack)
+		{
+			OperationNot<int> not;
+			Stack<int> s;
+			not.execute(s);
+
+			std::vector<int> expectedStack = { };
+			Assert::IsTrue(expectedStack == s.getStack());
+		}
+		TEST_METHOD(execute_OneElementOnStack)
+		{
+			OperationNot<int> not;
+			Stack<int> s;
+			s.push(4);
+			not.execute(s);
+
+			std::vector<int> expectedStack = { -5 };
+			Assert::IsTrue(expectedStack == s.getStack());
+		}
+		TEST_METHOD(execute_TwoElementsOnStack)
+		{
+			OperationNot<int> not;
+			Stack<int> s;
+			s.push(4);
+			s.push(-9);
+			not.execute(s);
+
+			std::vector<int> expectedStack = { 4, 8 };
+			Assert::IsTrue(expectedStack == s.getStack());
+		}
+		TEST_METHOD(execute_MoreThanTwoElementsOnStack)
+		{
+			OperationNot<int> not;
+			Stack<int> s;
+			s.push(5);
+			s.push(4);
+			s.push(12);
+			s.push(56);
+			s.push(3);
+			s.push(16);
+			not.execute(s);
+
+			std::vector<int> expectedStack = { 5, 4, 12, 56, 3, -17 };
+			Assert::IsTrue(expectedStack == s.getStack());
+		}
+	};
+}
