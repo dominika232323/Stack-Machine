eab55167ebcb74f7a4e71321fec9a0d182cde4bc
dominika232323 <dominikaboguszewska62@gmail.com>
2023-06-03 11:18:00 +0200

    Define execute in OperationAddition

--- StackMachine/StackMachineLib/OperationAddition.cpp
+++ StackMachine/StackMachineLib/OperationAddition.cpp
@@ -8,3 +8,11 @@ class OperationAddition : public Operation<T>
 public:
 	void execute(Stack<T>& s);
 };
+
+template<typename T>
+void OperationAddition<T>::execute(Stack<T>& s)
+{
+	T a = topAndPopOnStack(s);
+	T b = topAndPopOnStack(s);
+	s.push(a + b);
+}
