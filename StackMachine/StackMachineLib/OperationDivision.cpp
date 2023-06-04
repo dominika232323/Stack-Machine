c6037ef0cbb03cd2681a8178c36dbbeb3a273307
dominika232323 <dominikaboguszewska62@gmail.com>
2023-06-04 23:24:26 +0200

    Add and test operation division

--- StackMachine/StackMachineLib/OperationDivision.cpp
+++ StackMachine/StackMachineLib/OperationDivision.cpp
@@ -3,8 +3,30 @@
 
 
 template<typename T>
-class OperationDivison : public Operation<T>
+class OperationDivision : public Operation<T>
 {
 public:
-	void execute(Stack<T>& s);
+	void execute(Stack<T>& s)
+	{
+		if (canTopAndPopTwice(s))
+		{
+			T a = topAndPopOnStack(s);
+			
+			if (a == 0)
+				throw std::invalid_argument("Cannot devide by zero.");
+
+			T b = topAndPopOnStack(s);
+			s.push(b / a);
+		}
+		else if (!s.isEmpty())
+		{
+			topAndPopOnStack(s);
+			s.push(0);
+		}
+	}
+
+	std::string getName()
+	{
+		return "division";
+	}
 };
