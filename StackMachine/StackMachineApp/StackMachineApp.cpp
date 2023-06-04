1891b089696eac28591fe499c60fb5a746f0f150
dominika232323 <dominikaboguszewska62@gmail.com>
2023-06-04 14:59:36 +0200

    app

--- StackMachine/StackMachineApp/StackMachineApp.cpp
+++ StackMachine/StackMachineApp/StackMachineApp.cpp
@@ -1,4 +1,6 @@
 ﻿#include <iostream>
+#include <sstream>
+#include <string>
 #include "../StackMachineLib/StackMachine.cpp"
 
 
@@ -8,8 +10,25 @@ void createStackMachineChoice(int& choice)
 	std::cout << "Choose 2 to start a new stack machine." << std::endl;
 	std::cout << "Choose 3 to exit." << std::endl;
 	std::cout << "I choose: ";
-
 	std::cin >> choice;
+}
+
+
+template<typename T>
+void operateOnStackMachine(StackMachine<T>& stackMachine)
+{
+	std::cout << "Choose 1 to execute addition." << std::endl;
+	std::cout << "Choose 2 to execute subtraction." << std::endl;
+	std::cout << "Choose 3 to execute multiplication." << std::endl;
+	std::cout << "Choose 4 to execute divison." << std::endl;
+	std::cout << "Choose 5 to execute bitwise and." << std::endl;
+	std::cout << "Choose 6 to execute bitwise or." << std::endl;
+	std::cout << "Choose 7 to execute bitwise xor." << std::endl;
+	std::cout << "Choose 8 to execute bitwise not." << std::endl;
+	std::cout << "Choose 9 to execute swap." << std::endl;
+	std::cout << "Choose 10 to execute duplicate." << std::endl;
+	std::cout << "Choose 11 to execute operation from txt file." << std::endl;
+	std::cout << "Choose 12 to exit." << std::endl;
 }
 
 
@@ -23,6 +42,9 @@ int main()
 	StackMachine<double> stackMachine;
 	std::string filename;
 	bool correctChoice = false;
+
+	if (!isdigit(choice))
+		choice = 10;
 
 	while (correctChoice == false)
 	{
