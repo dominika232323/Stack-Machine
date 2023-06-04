5dbc12c89d5b63d57767525dffe9d89e093a75fc
dominika232323 <dominikaboguszewska62@gmail.com>
2023-06-05 01:51:50 +0200

    Add OperationAnd

---
+++ StackMachine/StackMachineLib/OperationAnd.cpp
@@ -0,0 +1,28 @@
+#pragma once
+#include "Operation.cpp"
+
+
+template<typename T>
+class OperationAnd : public Operation<T>
+{
+public:
+	void execute(Stack<T>& s)
+	{
+		if (canTopAndPopTwice(s))
+		{
+			T a = topAndPopOnStack(s);
+			T b = topAndPopOnStack(s);
+			s.push(a & b);
+		}
+		else if (!s.isEmpty())
+		{
+			topAndPopOnStack(s);
+			s.push(0);
+		}
+	}
+
+	std::string getName()
+	{
+		return "bitwise and";
+	}
+};
---
+++ StackMachine/StackMachineUnitTest/OperationAndUnitTest.cpp
@@ -0,0 +1,62 @@
+#include "CppUnitTest.h"
+#include "../StackMachineLib/OperationAnd.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace OperationAndUnitTest
+{
+	TEST_CLASS(OperationAndUnitTest)
+	{
+	public:
+		TEST_METHOD(getName)
+		{
+			OperationAnd<int> x;
+			Assert::AreEqual(std::string("bitwise and"), x.getName());
+		}
+		TEST_METHOD(execute_ZeroElementsOnStack)
+		{
+			OperationAnd<int> x;
+			Stack<int> s;
+			x.execute(s);
+
+			std::vector<int> expectedStack = { };
+			Assert::IsTrue(expectedStack == s.getStack());
+		}
+		TEST_METHOD(execute_OneElementOnStack)
+		{
+			OperationAnd<int> x;
+			Stack<int> s;
+			s.push(4);
+			x.execute(s);
+
+			std::vector<int> expectedStack = { 0 };
+			Assert::IsTrue(expectedStack == s.getStack());
+		}
+		TEST_METHOD(execute_TwoElementsOnStack)
+		{
+			OperationAnd<int> x;
+			Stack<int> s;
+			s.push(5);
+			s.push(4);
+			x.execute(s);
+
+			std::vector<int> expectedStack = { 4 };
+			Assert::IsTrue(expectedStack == s.getStack());
+		}
+		TEST_METHOD(execute_MoreThanTwoElementsOnStack)
+		{
+			OperationAnd<int> x;
+			Stack<int> s;
+			s.push(5);
+			s.push(4);
+			s.push(12);
+			s.push(56);
+			s.push(3);
+			s.push(16);
+			x.execute(s);
+
+			std::vector<int> expectedStack = { 5, 4, 12, 56, 0 };
+			Assert::IsTrue(expectedStack == s.getStack());
+		}
+	};
+}
