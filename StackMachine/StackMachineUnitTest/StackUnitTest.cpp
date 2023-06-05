f33e6d9fd32f44b50f6fd43c56209ae17acc7357
dominika232323 <dominikaboguszewska62@gmail.com>
2023-06-06 00:46:43 +0200

    Add clear_empty test

--- StackMachine/StackMachineUnitTest/StackUnitTest.cpp
+++ StackMachine/StackMachineUnitTest/StackUnitTest.cpp
@@ -42,6 +42,15 @@ namespace StackUnitTest
 			Assert::AreEqual(size_t(0), s.size());
 			Assert::IsTrue(s.isEmpty());
 		}
+		TEST_METHOD(clear_empty)
+		{
+			Stack<int> s;
+			s.clear();
+			std::vector<int> expected = {};
+			Assert::IsTrue(expected == s.getStack());
+			Assert::AreEqual(size_t(0), s.size());
+			Assert::IsTrue(s.isEmpty());
+		}
 		TEST_METHOD(push_toEmpty)
 		{
 			Stack<int> s;
